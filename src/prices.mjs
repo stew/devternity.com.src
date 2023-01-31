59ffbe0f365e2b389097cf33f20db91d87f91a34
Eduards Sizovs <eduards@devternity.com>
2023-01-31 18:46:43 +0800

    add debugging

--- src/prices.mjs
+++ src/prices.mjs
@@ -6,7 +6,9 @@ const toggleInputs = $(TOGGLE + ' input')
 export class CurrencySwitcher {
     init() {
         $(toggleInputs).on('change', () => {
+            console.log('Change triggered')
             const currency = $("input[name='currency']:checked").val();
+            console.log('Current  currency is ' + currency)
             $('[data-pricing]').each((index, pricing) => {
                 const pricingSection = $(pricing)
                 if (pricingSection.attr('data-pricing') === currency) {
