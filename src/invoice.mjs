126612c4a45a71e8a1e1f2b789fd64373f65f44d
Eduards Sizovs <eduards@devternity.com>
2023-03-11 14:19:58 +0800

    update uae invoice collection

--- src/invoice.mjs
+++ src/invoice.mjs
@@ -1,7 +1,5 @@
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
-import { v4 as uuid } from 'uuid';
 import $ from "cash-dom";
-import { object, string, array, number } from 'yup';
 import ky from 'ky';
 import dayjs from "dayjs";
 import advancedFormat from 'dayjs/plugin/advancedFormat.js'
@@ -23,7 +21,7 @@ export const app = createApp({
             return 'https://sales.devternity.ee/checkout/' + this.invoiceId;
         },
         fetchInvoice() {
-            ky("https://devternity-22e74.firebaseio.com/devplatforms-invoices/" + this.invoiceId + ".json").json()
+            ky("https://devternity-22e74.firebaseio.com/invoices/" + this.invoiceId + ".json").json()
             .then(response => {
                 this.invoice = response
             }).finally(() => {
